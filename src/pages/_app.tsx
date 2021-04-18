4a996b45a1bc9c946c2080be31a502ea2749541c
Dey-Sumit <sumax333@gmail.com>
2021-04-18 12:56:26 +0530

    created standard boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,7 @@
 import "tailwindcss/tailwind.css";
 import "@styles/globals.css";
-import Navbar from "@components/Navbar";
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="h-screen text-white">
-      <Navbar />
-      <div style={{ height: "90vh" }}>
-        <Component {...pageProps} />
-      </div>
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
